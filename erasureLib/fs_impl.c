a89e918b7a90f0dea2e66608da8b7dfa594f4d3d
Jeff Inman <jti@lanl.gov>
2017-07-26 10:25:57 -0600

    Avoid naming conflicts with libmarfs, in fs_impl.c

--- erasureLib/fs_impl.c
+++ erasureLib/fs_impl.c
@@ -72,6 +72,8 @@ GNU licenses can be found at http://www.gnu.org/licenses/.
 
 // ---------------------------------------------------------------------------
 // POSIX
+//
+// NOTE: We avoid naming conflicts with MarFS MDAL implementations.
 // ---------------------------------------------------------------------------
 
 
@@ -79,18 +81,18 @@ GNU licenses can be found at http://www.gnu.org/licenses/.
 #define FD(GFD)   (GFD)->fds.fd
 
 
-int     posix_fd_init(GenericFD* gfd) { FD(gfd) = -1; }
+int     fsi_posix_fd_init(GenericFD* gfd) { FD(gfd) = -1; }
 
-int     posix_fd_err(GenericFD* gfd) { return (FD(gfd) < 0); }
+int     fsi_posix_fd_err(GenericFD* gfd) { return (FD(gfd) < 0); }
 
-int     posix_fd_num(GenericFD* gfd) { return FD(gfd); }
+int     fsi_posix_fd_num(GenericFD* gfd) { return FD(gfd); }
 
 
-int     posix_auth_init(const char* user, SktAuth* auth) { return 0; }
+int     fsi_posix_auth_init(const char* user, SktAuth* auth) { return 0; }
 
-int     posix_auth_install(GenericFD* gfd, SktAuth* auth) { return 0; }
+int     fsi_posix_auth_install(GenericFD* gfd, SktAuth* auth) { return 0; }
 
-int     posix_open(GenericFD* gfd, const char* path, int flags, ...) {
+int     fsi_posix_open(GenericFD* gfd, const char* path, int flags, ...) {
    if (flags & O_CREAT) {
       va_list va;
       va_start(va, flags);
@@ -107,48 +109,48 @@ int     posix_open(GenericFD* gfd, const char* path, int flags, ...) {
 }
 
 
-ssize_t posix_write(GenericFD* gfd, const void* buf, size_t count) {
+ssize_t fsi_posix_write(GenericFD* gfd, const void* buf, size_t count) {
    return write(FD(gfd), buf, count);
 }
 
-ssize_t posix_read(GenericFD* gfd, void* buf, size_t count) {
+ssize_t fsi_posix_read(GenericFD* gfd, void* buf, size_t count) {
    return read(FD(gfd), buf, count);
 }
 
-int     posix_close(GenericFD* gfd) {
+int     fsi_posix_close(GenericFD* gfd) {
    return close(FD(gfd));
 }
 
 
 #if (AXATTR_SET_FUNC == 5) // XXX: not functional with threads!!!
-int     posix_fsetxattr(GenericFD* gfd, const char* name, const void* value, size_t size, int flags) {
+int     fsi_posix_fsetxattr(GenericFD* gfd, const char* name, const void* value, size_t size, int flags) {
    return fsetxattr(FD(gfd), name, value, size, flags);
 }
 
 #else
-int     posix_fsetxattr(GenericFD* gfd, const char *name, const void *value, size_t size, u_int32_t position, int options) {
+int     fsi_posix_fsetxattr(GenericFD* gfd, const char *name, const void *value, size_t size, u_int32_t position, int options) {
    return fsetxattr(FD(gfd), name, value, size, position, options);
 }
 #endif
 
 
 // #if (AXATTR_GET_FUNC == 4)
-// ssize_t posix_fgetxattr(GenericFD* gfd, const char* name, void* value, size_t size) {
+// ssize_t fsi_posix_fgetxattr(GenericFD* gfd, const char* name, void* value, size_t size) {
 //    return fgetxattr(FD(gfd), name, value, size);
 // }
 // 
 // #else
-// ssize_t posix_fgetxattr(GenericFD* gfd, const char *name, void *value, size_t size, u_int32_t position, int options) {
+// ssize_t fsi_posix_fgetxattr(GenericFD* gfd, const char *name, void *value, size_t size, u_int32_t position, int options) {
 //    return fgetxattr(FD(gfd), name, value, size, position, options);
 // }
 // #endif
 
 
-int     posix_fsync(GenericFD* gfd) {
+int     fsi_posix_fsync(GenericFD* gfd) {
    return fsync(FD(gfd));
 }
 
-off_t   posix_lseek(GenericFD* gfd, off_t offset, int whence) {
+off_t   fsi_posix_lseek(GenericFD* gfd, off_t offset, int whence) {
    return lseek(FD(gfd), offset, whence);
 }
 
@@ -156,16 +158,16 @@ off_t   posix_lseek(GenericFD* gfd, off_t offset, int whence) {
 
 
 // PATHOP
-int     posix_chown(SktAuth* auth, const char* path, uid_t owner, gid_t group) {
+int     fsi_posix_chown(SktAuth* auth, const char* path, uid_t owner, gid_t group) {
    return chown(path, owner, group);
 }
-int     posix_unlink(SktAuth* auth, const char* path) {
+int     fsi_posix_unlink(SktAuth* auth, const char* path) {
    return unlink(path);
 }
-int     posix_rename(SktAuth* auth, const char* oldpath, const char* newpath) {
+int     fsi_posix_rename(SktAuth* auth, const char* oldpath, const char* newpath) {
    return rename(oldpath, newpath);
 }
-int     posix_stat(SktAuth* auth, const char* path, struct stat* buff) {
+int     fsi_posix_stat(SktAuth* auth, const char* path, struct stat* buff) {
    return stat(path, buff);
 }
 
@@ -174,27 +176,27 @@ int     posix_stat(SktAuth* auth, const char* path, struct stat* buff) {
 
 
 static const FileSysImpl posix_impl = {
-   .fd_init      = &posix_fd_init,
-   .fd_err       = &posix_fd_err,
-   .fd_num       = &posix_fd_num,
+   .fd_init      = &fsi_posix_fd_init,
+   .fd_err       = &fsi_posix_fd_err,
+   .fd_num       = &fsi_posix_fd_num,
 
-   .auth_init    = &posix_auth_init,
-   .auth_install = &posix_auth_install,
+   .auth_init    = &fsi_posix_auth_init,
+   .auth_install = &fsi_posix_auth_install,
 
-   .open         = &posix_open,
-   .write        = &posix_write,
-   .read         = &posix_read,
-   .close        = &posix_close,
-   .fsetxattr    = &posix_fsetxattr,
-   // .fgetxattr    = &posix_fgetxattr,
-   .fsync        = &posix_fsync,
-   .lseek        = &posix_lseek,
+   .open         = &fsi_posix_open,
+   .write        = &fsi_posix_write,
+   .read         = &fsi_posix_read,
+   .close        = &fsi_posix_close,
+   .fsetxattr    = &fsi_posix_fsetxattr,
+   // .fgetxattr    = &fsi_posix_fgetxattr,
+   .fsync        = &fsi_posix_fsync,
+   .lseek        = &fsi_posix_lseek,
 
    // path-ops
-   .chown        = &posix_chown,
-   .unlink       = &posix_unlink,
-   .rename       = &posix_rename,
-   .stat         = &posix_stat,
+   .chown        = &fsi_posix_chown,
+   .unlink       = &fsi_posix_unlink,
+   .rename       = &fsi_posix_rename,
+   .stat         = &fsi_posix_stat,
 };
 
 
@@ -210,6 +212,8 @@ const FileSysImpl* fs_impl_posix = &posix_impl;
 
 // ---------------------------------------------------------------------------
 // SOCKETS
+//
+// NOTE: We avoid naming conflicts with librdma_sockets implementations.
 // ---------------------------------------------------------------------------
 
 
@@ -217,24 +221,24 @@ const FileSysImpl* fs_impl_posix = &posix_impl;
 #define SKT(GFD)   (&(GFD)->fds.skt)
 
 
-int sockets_fd_init(GenericFD* gfd) { SKT(gfd)->peer_fd = -1; return 0; }
+int fsi_skt_fd_init(GenericFD* gfd) { SKT(gfd)->peer_fd = -1; return 0; }
 
-int sockets_fd_err(GenericFD* gfd)  { return (SKT(gfd)->peer_fd < 0); }
+int fsi_skt_fd_err(GenericFD* gfd)  { return (SKT(gfd)->peer_fd < 0); }
 
-int sockets_fd_num(GenericFD* gfd)  { return SKT(gfd)->peer_fd; }
+int fsi_skt_fd_num(GenericFD* gfd)  { return SKT(gfd)->peer_fd; }
 
 
 
-int sockets_auth_init(const char* user, SktAuth* auth) {
+int fsi_skt_auth_init(const char* user, SktAuth* auth) {
    return skt_auth_init(user, auth);
 }
-int sockets_auth_install(GenericFD* gfd, SktAuth* auth) {
+int fsi_skt_auth_install(GenericFD* gfd, SktAuth* auth) {
    return skt_auth_install(SKT(gfd), auth);
 }
 
 
 
-int sockets_open(GenericFD* gfd, const char* path, int flags, ...) {
+int fsi_skt_open(GenericFD* gfd, const char* path, int flags, ...) {
 
    SocketHandle* skt = SKT(gfd);
 
@@ -262,48 +266,48 @@ int sockets_open(GenericFD* gfd, const char* path, int flags, ...) {
 }
 
 
-ssize_t sockets_write(GenericFD* gfd, const void* buf, size_t count) {
+ssize_t fsi_skt_write(GenericFD* gfd, const void* buf, size_t count) {
    return skt_write(SKT(gfd), buf, count);
 }
 
-ssize_t sockets_read(GenericFD* gfd, void* buf, size_t count) {
+ssize_t fsi_skt_read(GenericFD* gfd, void* buf, size_t count) {
    return skt_read(SKT(gfd), buf, count);
 }
 
-int     sockets_close(GenericFD* gfd) {
+int     fsi_skt_close(GenericFD* gfd) {
    return skt_close(SKT(gfd));
 }
 
 
 #if (AXATTR_SET_FUNC == 5) // XXX: not functional with threads!!!
-int     sockets_fsetxattr(GenericFD* gfd, const char* name, const void* value, size_t size, int flags) {
+int     fsi_skt_fsetxattr(GenericFD* gfd, const char* name, const void* value, size_t size, int flags) {
    return skt_fsetxattr(SKT(gfd), name, value, size, flags);
 }
 
 #else
-int     sockets_fsetxattr(GenericFD* gfd, const char *name, const void *value, size_t size, u_int32_t position, int options) {
+int     fsi_skt_fsetxattr(GenericFD* gfd, const char *name, const void *value, size_t size, u_int32_t position, int options) {
    return skt_fsetxattr(SKT(gfd), name, value, size, position, options);
 }
 #endif
 
 
 // #if (AXATTR_GET_FUNC == 4)
-// ssize_t sockets_fgetxattr(GenericFD* gfd, const char* name, void* value, size_t size) {
+// ssize_t fsi_skt_fgetxattr(GenericFD* gfd, const char* name, void* value, size_t size) {
 //    return skt_fgetxattr(SKT(gfd), name, value, size);
 // }
 // 
 // #else
-// ssize_t sockets_fgetxattr(GenericFD* gfd, const char *name, void *value, size_t size, u_int32_t position, int options) {
+// ssize_t fsi_skt_fgetxattr(GenericFD* gfd, const char *name, void *value, size_t size, u_int32_t position, int options) {
 //    return skt_fgetxattr(SKT(gfd), name, value, size, position, options);
 // }
 // #endif
 
 
-int     sockets_fsync(GenericFD* gfd) {
+int     fsi_skt_fsync(GenericFD* gfd) {
    return skt_fsync(SKT(gfd));
 }
 
-off_t   sockets_lseek(GenericFD* gfd, off_t offset, int whence) {
+off_t   fsi_skt_lseek(GenericFD* gfd, off_t offset, int whence) {
    return skt_lseek(SKT(gfd), offset, whence);
 }
 
@@ -311,19 +315,19 @@ off_t   sockets_lseek(GenericFD* gfd, off_t offset, int whence) {
 
 
 // PATHOP
-int     sockets_chown(SktAuth* auth, const char* path, uid_t owner, gid_t group) {
+int     fsi_skt_chown(SktAuth* auth, const char* path, uid_t owner, gid_t group) {
    return skt_chown(auth, path, owner, group);
 }
 
-int     sockets_unlink(SktAuth* auth, const char* path) {
+int     fsi_skt_unlink(SktAuth* auth, const char* path) {
    return skt_unlink(auth, path);
 }
 
-int     sockets_rename(SktAuth* auth, const char* oldpath, const char* newpath) {
+int     fsi_skt_rename(SktAuth* auth, const char* oldpath, const char* newpath) {
    return skt_rename(auth, oldpath, newpath);
 }
 
-int     sockets_stat(SktAuth* auth, const char* path, struct stat* buff) {
+int     fsi_skt_stat(SktAuth* auth, const char* path, struct stat* buff) {
    return skt_stat(auth, path, buff);
 }
 
@@ -332,27 +336,27 @@ int     sockets_stat(SktAuth* auth, const char* path, struct stat* buff) {
 
 
 static const FileSysImpl sockets_impl = {
-   .fd_init      = &sockets_fd_init,
-   .fd_err       = &sockets_fd_err,
-   .fd_num       = &sockets_fd_num,
+   .fd_init      = &fsi_skt_fd_init,
+   .fd_err       = &fsi_skt_fd_err,
+   .fd_num       = &fsi_skt_fd_num,
 
-   .auth_init    = &sockets_auth_init,
-   .auth_install = &sockets_auth_install,
+   .auth_init    = &fsi_skt_auth_init,
+   .auth_install = &fsi_skt_auth_install,
 
-   .open         = &sockets_open,
-   .write        = &sockets_write,
-   .read         = &sockets_read,
-   .close        = &sockets_close,
-   .fsetxattr    = &sockets_fsetxattr,
-   // .fgetxattr    = &sockets_fgetxattr,
-   .fsync        = &sockets_fsync,
-   .lseek        = &sockets_lseek,
+   .open         = &fsi_skt_open,
+   .write        = &fsi_skt_write,
+   .read         = &fsi_skt_read,
+   .close        = &fsi_skt_close,
+   .fsetxattr    = &fsi_skt_fsetxattr,
+   // .fgetxattr    = &fsi_skt_fgetxattr,
+   .fsync        = &fsi_skt_fsync,
+   .lseek        = &fsi_skt_lseek,
 
    // path-ops
-   .chown        = &sockets_chown,
-   .unlink       = &sockets_unlink,
-   .rename       = &sockets_rename,
-   .stat         = &sockets_stat,
+   .chown        = &fsi_skt_chown,
+   .unlink       = &fsi_skt_unlink,
+   .rename       = &fsi_skt_rename,
+   .stat         = &fsi_skt_stat,
    
 };
 
